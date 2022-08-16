2406f6f989dd90ad7ffa75473aa4d852022dbb35
Michael Ranciglio <rancigliom@gmail.com>
2022-08-15 22:40:26 -0500

    Set up lint

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+};
